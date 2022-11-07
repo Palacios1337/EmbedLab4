d9cecbce2202726852106a68697fddbe0c01041d
Palacios1337 <98562237+Palacios1337@users.noreply.github.com>
2022-11-07 00:19:46 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -5,12 +5,12 @@ int main(void)
     P1DIR |= BIT6 | BIT7;                     // P1.6 and P1.7 output
     P1SEL1 |= BIT6 | BIT7;                    // P1.6 and P1.7 options select
     PM5CTL0 &= ~LOCKLPM5;
-    TB0CCR0 = 16384-1;                         // PWM Period
+    TB0CCR0 = 62500;                         // PWM Period
     TB0CCTL1 = OUTMOD_7;                      // CCR1 reset/set
-    TB0CCR1 = 1638;                            // CCR1 PWM duty cycle
-    TB0CTL = TBSSEL__ACLK | MC__UP | TBCLR;  // ACLK, up mode, clear TBR
+    TB0CCR1 = 12500;                            // CCR1 PWM duty cycle
+    TB0CTL = TBSSEL__SMCLK|ID_2 | MC__UP | TBCLR;  // SMCLK,clock divider of 4, up mode, clear TBR
     while(1)
     {
-        P1OUT ^=BIT6; // This allows to see the PWM signal
+        P1OUT ^=BIT6; // PWM signal out
     }
 }
